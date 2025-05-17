861a9d5f708a4fb9516f71c8b5397162d3633466
oriree70 <41202028+oriree70@users.noreply.github.com>
2025-05-16 20:12:44 -0400

    Update app.js
    Changed the port to listen to forward traffic to 0.0.0.0 instead of forwarding traffic to only localhost.

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.get('/', (req, res) => {
   res.send(html);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
